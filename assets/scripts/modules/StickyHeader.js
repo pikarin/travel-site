b567c09025cbb48255700ccd098aab23b68ab6a4
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2017-07-09 11:07:13 +0700

    Completed sticky header functionality

---
+++ assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,65 @@
+import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class StickyHeader {
+  constructor() {
+    this.siteHeader = $(".site-header");
+    this.headerTriggerElement = $(".large-hero__title");
+    this.pageSections = $(".page-section");
+    this.headerLinks = $(".primary-nav a");
+    this.createHeaderWaypoint();
+    this.createPageSectionsWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
+  }
+
+  createHeaderWaypoint() {
+    var that = this;
+    new Waypoint({
+      element: that.headerTriggerElement[0],
+      handler: function(direction) {
+        if (direction == "down") {
+          that.siteHeader.addClass("site-header--dark");
+        } else {
+          that.siteHeader.removeClass("site-header--dark");
+        }
+      }
+    });
+  }
+
+  createPageSectionsWaypoints() {
+    var that = this;
+    this.pageSections.each(function() {
+      var currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        offset: "18%",
+        handler: function(direction) {
+          if (direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        }
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        offset: "-40%",
+        handler: function(direction) {
+          if (direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        }
+      });
+    });
+  }
+}
+
+export default StickyHeader;
